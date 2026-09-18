f126b3707271dc51f82a2eb8b69fee162b4e18a6
Paulo Vieira <paulo_vieira22@hotmail.com>
2026-09-18 15:15:20 -0300

    Revert "modulo 8"
    This reverts commit e72dc6c86b4f1a19ea5097f4889091824cccdd3f.

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,11 +1,7 @@
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
-
 
 
 # Quick-start development settings - unsuitable for production
@@ -47,7 +43,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIRS],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
