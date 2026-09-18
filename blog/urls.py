a4dfa177b225de7dc3dceb41bc8445b1bdf89eb5
Paulo Vieira <paulo_vieira22@hotmail.com>
2026-09-18 14:30:15 -0300

    modulo 8

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("home/", views.PostView.as_view(), name="home")
+    path("home/", views.PostView.as_view(), name="home"),
+    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
